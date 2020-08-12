6813d5af84d8b67d2dd99d6692ce250cb6b9e4f6
afd <afd@alup5.academiapostal.es.local>
2020-08-12 20:34:31 +0200

    pab3890

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { ProyectoListComponent } from './proyecto-list/proyecto-list.component';
 import { TareasListComponent } from './tareas-list/tareas-list.component';
 import { UsuariosListComponent } from './usuarios-list/usuarios-list.component';
 import { RolesListComponent } from './roles-list/roles-list.component';
+import { ProyectoDetailComponent } from './proyecto-detail/proyecto-detail.component';
 
 const routes: Routes = [
   {path: 'proyectos', component: ProyectoListComponent},
+  {path: 'proyectos/:id', component: ProyectoDetailComponent},
   {path: 'tareas', component: TareasListComponent},
   {path: 'usuarios', component: UsuariosListComponent},
   {path: 'roles', component: RolesListComponent}

883dbcf71994bda98022c450f3ee65bff6f93c9b
afd <afd@alup5.academiapostal.es.local>
2020-08-13 18:47:15 +0200

    pab3890

--- src/app/proyecto-detail/proyecto-detail.component.ts
+++ src/app/proyecto-detail/proyecto-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-proyecto-detail',
@@ -11,15 +12,31 @@ import { environment } from 'src/environments/environment';
 export class ProyectoDetailComponent implements OnInit {
 
   proyectoForm = this.fb.group({
+    id: null,
     nombre: [null, Validators.required],
     descripcion: null,
     fechaInicio: [null, Validators.required],
     fechaFin: null
   })
 
-  constructor(private fb: FormBuilder, private http: HttpClient) { }
+  id = undefined;
+
+  constructor(private fb: FormBuilder, private http: HttpClient, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      if (params.id !== "_nuevo") {
+        this.http.get(environment.apiBaseUrl + "proyectos/" + params.id)
+          .subscribe(
+            proyecto => {
+              this.proyectoForm.patchValue(proyecto);
+            },
+            error => {
+              alert("No se pudo cargar el proyecto");
+            }
+          )
+      }
+    })
   }
 
   crear() {
@@ -32,4 +49,33 @@ export class ProyectoDetailComponent implements OnInit {
           alert("Error creando el proyecto: " + error.message)
         })
   }
+
+  modificar() {
+    this.http.put(
+      environment.apiBaseUrl + "proyectos/" + this.id,
+      this.proyectoForm.value)
+    .subscribe(
+      proyecto => {
+        alert("Proyecto actualizado");
+      },
+      error => {
+        alert("Error actualizando proyecto: " + error.message);
+      }
+    )
+  }
+
+  eliminar() {
+    if (confirm("¿Está seguro?")) {
+      this.http.delete<any>(environment.apiBaseUrl + "proyectos/" + this.id)
+      .subscribe(
+        () => {
+          alert("Proyecto eliminado con éxito.");
+        },
+        error => {
+          alert("Error eliminado el proyecto: " + error.message);
+        }
+      )
+    }
+  }
+
 }
